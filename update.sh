94d6c95d93375decd60faa1f5f7951479857b84c
terminalPoltergeist <nemit031@umn.edu>
2021-03-22 15:22:45 -0500

    auto commit

---
+++ update.sh
@@ -0,0 +1,16 @@
+# Update App Store apps
+sudo softwareupdate -i -a
+# Update Homebrew (Cask) & packages
+brew update
+brew upgrade
+# Update npm & packages
+npm install npm -g
+npm update -g
+# Update the chromedriver
+brew upgrade chromedriver
+# Update git
+brew upgrade git
+# Update pip
+pip install --upgrade pip
+# Update python3
+brew install python3
