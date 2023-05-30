b70128e3b78c93ba7ba816f0be9981034d077e69
terminalPoltergeist <jack.nemitz@yahoo.com>
2023-05-30 09:14:24 -0500

    update: theme
    |What:
      - switch from nord to custom gruvbox

--- sketchybar/colors.sh
+++ sketchybar/colors.sh
@@ -1,27 +1,17 @@
-#!/bin/sh
-
-# Start flavours
-export BLACK=0xff2E3440
-export WHITE=0xffECEFF4
-export RED=0xffBF616A
-export GREEN=0xffA3BE8C
-export BLUE=0xff88C0D0
-export YELLOW=0xffEBCB8B
-export ORANGE=0xffD08770
-export MAGENTA=0xffB48EAD
-export GREY=0xffD8DEE9
+export BLACK=0xff323232
+export WHITE=0xffe0d0af
+export RED=0xffd76b6d
+export GREEN=0xffb4c08f
+export BLUE=0xff85ad85
+export YELLOW=0xffffd780
+export ORANGE=0xffffc37f
+export MAGENTA=0xffd485ad
+export GREY=0xff9e9e9e
 export TRANSPARENT=0x00000000
 
 # General bar colors
-export BAR_COLOR=0xff4C566A
+export BAR_COLOR=0xff5a5a5a
 export ICON_COLOR=$WHITE # Color of all icons
 export LABEL_COLOR=$WHITE # Color of all labels
-export BACKGROUND_1=0xBF3B4252
-export BACKGROUND_2=0xff434C5E
-
-# End flavours
-
-export POPUP_BACKGROUND_COLOR=$BLACK
-export POPUP_BORDER_COLOR=$WHITE
-
-export SHADOW_COLOR=$BLACK
+export BACKGROUND_1=0xBF404040
+export BACKGROUND_2=0xff4e4e4e
